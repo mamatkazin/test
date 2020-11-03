0379bfc0de1f28cccca817076e0ae36dbfa26ef1
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2020-11-03 13:59:05 +0300

    1

--- go.mod
+++ go.mod
@@ -3,14 +3,12 @@ module hospital_track
 go 1.13
 
 require (
-	github.com/Moriska32/telegram-bot-messenger v0.0.0-20200911113925-ee7f397ddb00 // indirect
 	github.com/appleboy/gin-jwt/v2 v2.6.4
 	github.com/elgs/gosqljson v0.0.0-20160403005647-027aa4915315 // indirect
 	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-pg/pg v8.0.7+incompatible
-	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/lib/pq v1.8.0
 	github.com/onsi/ginkgo v1.14.1 // indirect
--- main.go
+++ main.go
@@ -11,32 +11,6 @@ import (
 	"hospital_track/service"
 )
 
-// type S_Status struct {
-// 	Server bool        `json:"server"`
-// 	DB     bool        `json:"db"`
-// 	TI     interface{} `json:"time_idle"`
-// }
-
-// type S_Body struct {
-// 	IMEI      int64   `json:"imei"`
-// 	MAC       string  `json:"mac"`
-// 	TS        int64   `json:"timestamp"`
-// 	Lat       float64 `json:"lat"`
-// 	Lng       float64 `json:"lng"`
-// 	Speed     float64 `json:"speed"`
-// 	Length    float64 `json:"length"`
-// 	Hindrace  float64 `json:"distObject"`
-// 	Direction float64 `json:"ori"`
-// }
-
-// type S_Answer_Ins struct {
-// 	Valid bool `json:"valid"`
-// 	//	Hindrace bool  `json:"hindrace"`
-// 	TS    int64 `json:"timestamp"`
-// 	TL    bool  `json:"traffic_light"`
-// 	TLNII bool  `json:"traffic_light_nii"`
-// }
-
 func init() {
 	if err := initConfigFile(); err != nil {
 		return
@@ -51,7 +25,6 @@ func main() {
 	}()
 
 	var (
-		//mx    *mux.Router
 		err   error
 		repos *repository.SRepository
 	)
@@ -66,119 +39,13 @@ func main() {
 
 	http := handlers.InitRoutes()
 
-	// if err = common.ConnectDB(0); err != nil {
-	// 	panic(err.Error())
-	// }
-
 	log.Printf("success: port=%s\n", env.GCONFIG.AppPort)
-
-	// mx = mux.NewRouter()
-
-	// sp := http.StripPrefix("/", http.FileServer(http.Dir("./")))
-
-	// mx.HandleFunc("/api/tracks", trackHandler)
-
-	// mx.PathPrefix("/").Handler(sp)
 
 	if err = http.Run(env.GCONFIG.AppPort); err != nil {
 		log.Printf("panic: Не возомжно стартануть сервер. Ошибка: %s", err.Error())
 		return
 	}
 }
-
-// func trackHandler(w http.ResponseWriter, r *http.Request) {
-// 	var (
-// 		bodyBytes []byte
-// 		err       error
-// 		bodySTR   string
-// 		body      S_Body
-// 		oID, oTID int64
-// 		tm        time.Time
-// 		//res          S_Answer_Ins
-// 	)
-
-// 	defer func() {
-// 		if rec := recover(); rec != nil {
-
-// 			// fmt.Println(fmt.Sprintf("Error trackHandler: %v", rec))
-
-// 			// res.Valid = false
-// 			// res.TS = time.Now().Unix()
-
-// 			// if b, err = json.Marshal(res); err != nil {
-// 			// 	b = []byte(common.ProcessingError(err.Error()))
-// 			// }
-
-// 			// w.Write(b)
-// 		}
-// 	}()
-
-// 	if r.Method == "POST" {
-
-// 		if bodyBytes, err = ioutil.ReadAll(r.Body); err != nil {
-// 			panic("Error reading body: " + err.Error())
-// 		}
-
-// 		bodySTR = string(bodyBytes)
-
-// 		if err = json.Unmarshal([]byte(bodySTR), &body); err != nil {
-// 			panic(err.Error())
-// 		}
-
-// 		tm = time.Unix(0, body.TS*int64(time.Millisecond))
-
-// 		fmt.Println(tm, r.Method, body.MAC, body.TS, body.Lng, body.Lat, body.Speed, body.Length*1000, math.Round(body.Hindrace), math.Round(body.Direction))
-
-// 		if _, err = common.G_INS.QueryOne(pg.Scan(&oID), tm, body.MAC, body.Lng, body.Lat, body.Speed, body.Length*1000, math.Round(body.Hindrace), math.Round(body.Direction)); err != nil {
-// 			if !common.CheckDB() {
-// 				if err = common.ConnectDB(0); err != nil {
-// 					panic(err.Error())
-// 				}
-// 			}
-
-// 			panic(err.Error())
-// 		}
-
-// 		if oID > 0 {
-// 			if _, err = common.G_COMPUTED.QueryOne(pg.Scan(&oTID), oID); err != nil {
-// 				if !common.CheckDB() {
-// 					if err = common.ConnectDB(0); err != nil {
-// 						panic(err.Error())
-// 					}
-// 				}
-
-// 				panic(err.Error())
-// 			}
-
-// 			// res.Valid = true
-
-// 			fmt.Println("oTID", oTID)
-
-// 		} else {
-// 			//res.Valid = false
-// 		}
-
-// 		fmt.Println("Reading oID: ", oID)
-
-// 		// res.TS = time.Now().Unix()
-
-// 		// if b, err = json.Marshal(res); err != nil {
-// 		// 	panic(common.ProcessingError(err.Error()))
-// 		// }
-
-// 		// w.Write(b)
-// 	} else {
-// 		// res.Valid = false
-// 		// res.TS = time.Now().Unix()
-
-// 		// if b, err = json.Marshal(res); err != nil {
-// 		// 	b = []byte(common.ProcessingError(err.Error()))
-// 		// }
-
-// 		// w.Write(b)
-// 	}
-
-// }
 
 func initConfigFile() (err error) {
 	f, err := os.Open("config.json")
